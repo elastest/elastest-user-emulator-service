83cd430b1eaaa9ec7a009e95b5c1496dd1e98026
Boni Garcia <bgarcia@gsyc.es>
2017-06-08 17:04:48 +0200

    Include license header and minimal javadoc in all Java classes

--- elastest-eus/src/test/java/io/elastest/eus/test/dummy/DummyTest.java
+++ elastest-eus/src/test/java/io/elastest/eus/test/dummy/DummyTest.java
@@ -1,3 +1,19 @@
+/*
+ * (C) Copyright 2017-2019 ElasTest (http://elastest.io/)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
 package io.elastest.eus.test.dummy;
 
 import static org.junit.Assert.assertTrue;
@@ -6,6 +22,12 @@ import org.junit.jupiter.api.Test;
 import org.junit.platform.runner.JUnitPlatform;
 import org.junit.runner.RunWith;
 
+/**
+ * Dummy test.
+ *
+ * @author Boni Garcia (boni.garcia@urjc.es)
+ * @since 0.0.1
+ */
 @RunWith(JUnitPlatform.class)
 public class DummyTest {
 
