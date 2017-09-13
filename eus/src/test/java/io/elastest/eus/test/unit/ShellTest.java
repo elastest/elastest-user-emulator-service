205621626843bdf68ff2ae8f9375c1679bef4874
Boni Garcia <bgarcia@gsyc.es>
2017-09-13 15:37:22 +0200

    Include unit test for ShellService

---
+++ eus/src/test/java/io/elastest/eus/test/unit/ShellTest.java
@@ -0,0 +1,61 @@
+/*
+ * (C) Copyright 2017-2019 ElasTest (http://elastest.io/)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+package io.elastest.eus.test.unit;
+
+import static org.hamcrest.CoreMatchers.not;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.text.IsEmptyString.isEmptyString;
+import static org.junit.jupiter.api.TestInstance.Lifecycle.PER_CLASS;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import io.elastest.eus.service.ShellService;
+import io.elastest.eus.test.util.MockitoExtension;
+
+/**
+ * Tests for shell service.
+ *
+ * @author Boni Garcia (boni.garcia@urjc.es)
+ * @since 0.1.1
+ */
+@ExtendWith(MockitoExtension.class)
+@TestInstance(PER_CLASS)
+@Tag("unit")
+@DisplayName("Unit tests Shell Service")
+public class ShellTest {
+
+    final Logger log = LoggerFactory.getLogger(ShellTest.class);
+
+    @InjectMocks
+    ShellService shellService;
+
+    @Test
+    @DisplayName("List current folder")
+    void test() {
+        String dirOutput = shellService.runAndWait("dir");
+        log.debug("The list of the current folder is: {}", dirOutput);
+        assertThat(dirOutput, not(isEmptyString()));
+    }
+
+}
