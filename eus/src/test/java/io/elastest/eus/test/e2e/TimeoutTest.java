2d212ef7cdb3c6c181bb2c3e950dae43b40e5a70
Boni Garcia <bgarcia@gsyc.es>
2017-09-15 10:28:50 +0200

    Include E2E test for timeout

---
+++ eus/src/test/java/io/elastest/eus/test/e2e/TimeoutTest.java
@@ -0,0 +1,79 @@
+/*
+ * (C) Copyright 2017-2019 ElasTest (http://elastest.io/)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+package io.elastest.eus.test.e2e;
+
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.springframework.boot.test.context.SpringBootTest.WebEnvironment.RANDOM_PORT;
+
+import java.net.URL;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.openqa.selenium.Capabilities;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.test.context.TestPropertySource;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+/**
+ * Timeout test.
+ *
+ * @author Boni Garcia (boni.garcia@urjc.es)
+ * @since 0.0.1
+ */
+@ExtendWith(SpringExtension.class)
+@SpringBootTest(webEnvironment = RANDOM_PORT)
+@TestPropertySource(properties = { "hub.timeout=0",
+        "novnc.image.id=elastest/eus-novnc" })
+public class TimeoutTest {
+
+    final Logger log = LoggerFactory.getLogger(TimeoutTest.class);
+
+    WebDriver driver;
+
+    @LocalServerPort
+    int serverPort;
+
+    @Value("${server.servlet.context-path}")
+    String contextPath;
+
+    @Test
+    void testTimeout() {
+        assertThrows(Exception.class, () -> {
+            String eusUrl = "http://localhost:" + serverPort + contextPath;
+            Capabilities capabilities = DesiredCapabilities.chrome();
+            driver = new RemoteWebDriver(new URL(eusUrl), capabilities);
+            log.debug("EUS URL: {}", eusUrl);
+            driver.get("http://elastest.io/");
+        });
+    }
+
+    @AfterEach
+    void teardown() {
+        if (driver != null) {
+            driver.quit();
+        }
+    }
+
+}
