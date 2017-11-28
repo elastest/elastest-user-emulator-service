293be9842d230f63dfa46137895b48f408adcf80
Boni Garcia <bgarcia@gsyc.es>
2017-11-28 12:39:35 +0100

    Fix script for Dockerfile generation

--- browser-firefox/generate.sh
+++ browser-firefox/generate.sh
@@ -4,9 +4,9 @@ VERSION=$1
 echo "###########################################" > ./Dockerfile
 echo "# Generated file with script (generate.sh)" >> ./Dockerfile
 echo "# Do not commit these changes!" >> ./Dockerfile
-echo "###########################################" > ./Dockerfile
+echo "###########################################" >> ./Dockerfile
 
-echo FROM selenium/standalone-firefox-debug:$VERSION
-echo USER root
-echo RUN apt-get update && apt-get install -y pulseaudio
-echo USER seluser
+echo FROM selenium/standalone-firefox-debug:$VERSION >> ./Dockerfile
+echo USER root >> ./Dockerfile
+echo RUN apt-get update && apt-get install -y pulseaudio >> ./Dockerfile
+echo USER seluser >> ./Dockerfile
