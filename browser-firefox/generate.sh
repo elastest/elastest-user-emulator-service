fdb50f52556cff68ab3ad3b24acb18d478d68abf
Boni Garcia <bgarcia@gsyc.es>
2017-11-28 12:46:18 +0100

    Escape ampersand in echo command

--- browser-firefox/generate.sh
+++ browser-firefox/generate.sh
@@ -7,6 +7,6 @@ echo "# Do not commit these changes!" >> ./Dockerfile
 echo "###########################################" >> ./Dockerfile
 
 echo FROM selenium/standalone-firefox-debug:$VERSION >> ./Dockerfile
-echo USER root >> ./Dockerfile
-echo RUN apt-get update && apt-get install -y pulseaudio >> ./Dockerfile
-echo USER seluser >> ./Dockerfile
+echo "USER root" >> ./Dockerfile
+echo "RUN apt-get update && apt-get install -y pulseaudio" >> ./Dockerfile
+echo "USER seluser" >> ./Dockerfile
