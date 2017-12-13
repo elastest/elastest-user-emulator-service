f5fa0f59fd58eee983a9e723feea34f8ff2db61c
franciscoRdiaz <francisco.diazbayon@gmail.com>
2017-12-13 11:22:36 +0100

    chore: Add the labels to the script used to generate the Dockerfile

--- browser-firefox/generate.sh
+++ browser-firefox/generate.sh
@@ -7,6 +7,12 @@ echo "# Do not commit these changes!" >> ./Dockerfile
 echo "###########################################" >> ./Dockerfile
 
 echo FROM selenium/standalone-firefox-debug:$VERSION >> ./Dockerfile
+echo ARG GIT_COMMIT=unspecified >> ./Dockerfile
+echo LABEL git_commit=$GIT_COMMIT >> ./Dockerfile
+echo ARG COMMIT_DATE=unspecified >> ./Dockerfile
+echo LABEL commit_date=$COMMIT_DATE >> ./Dockerfile
+echo ARG VERSION=unspecified >> ./Dockerfile
+echo LABEL version=$VERSION >> ./Dockerfile 
 echo "USER root" >> ./Dockerfile
 echo "RUN apt-get update && apt-get install -y pulseaudio" >> ./Dockerfile
 echo "USER seluser" >> ./Dockerfile
