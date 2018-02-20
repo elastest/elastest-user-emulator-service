f06d7d0e6cd87c72fd591041c761b23f8b37bda2
Boni Garcia <bgarcia@gsyc.es>
2018-02-20 18:25:39 +0100

    Reduce time wait to 20 sec in TimeoutTest

--- eus/src/test/java/io/elastest/eus/test/e2e/TimeoutTest.java
+++ eus/src/test/java/io/elastest/eus/test/e2e/TimeoutTest.java
@@ -72,7 +72,7 @@ public class TimeoutTest {
 
     @Test
     void testTimeout() throws InterruptedException {
-        long waitSeconds = 30;
+        long waitSeconds = 20;
         log.debug("Waiting {} seconds to force timeout", waitSeconds);
         SECONDS.sleep(waitSeconds);
 
