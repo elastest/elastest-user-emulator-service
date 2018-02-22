afcbafeb21fa84ffba9e761a683cc01d08ab0d52
Boni Garcia <bgarcia@gsyc.es>
2018-02-22 12:27:12 +0100

    Include integration test for Logstash service

---
+++ eus/src/test/java/io/elastest/eus/test/integration/LogstashIntegrationTest.java
@@ -0,0 +1,110 @@
+/*
+ * (C) Copyright 2017-2019 ElasTest (http://elastest.io/)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+package io.elastest.eus.test.integration;
+
+import static com.github.tomakehurst.wiremock.client.WireMock.aResponse;
+import static com.github.tomakehurst.wiremock.client.WireMock.equalTo;
+import static com.github.tomakehurst.wiremock.client.WireMock.post;
+import static com.github.tomakehurst.wiremock.client.WireMock.postRequestedFor;
+import static com.github.tomakehurst.wiremock.client.WireMock.stubFor;
+import static com.github.tomakehurst.wiremock.client.WireMock.urlEqualTo;
+import static com.github.tomakehurst.wiremock.client.WireMock.verify;
+import static com.github.tomakehurst.wiremock.core.WireMockConfiguration.options;
+import static java.lang.invoke.MethodHandles.lookup;
+import static org.junit.jupiter.api.TestInstance.Lifecycle.PER_CLASS;
+import static org.slf4j.LoggerFactory.getLogger;
+import static org.springframework.boot.test.context.SpringBootTest.WebEnvironment.RANDOM_PORT;
+
+import java.net.ServerSocket;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.internal.util.reflection.FieldSetter;
+import org.slf4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import com.github.tomakehurst.wiremock.WireMockServer;
+import com.github.tomakehurst.wiremock.client.WireMock;
+
+import io.elastest.eus.service.LogstashService;
+
+/**
+ * Tests for Logstash service.
+ *
+ * @author Boni Garcia (boni.garcia@urjc.es)
+ * @since 0.5.1
+ */
+@ExtendWith(SpringExtension.class)
+@TestInstance(PER_CLASS)
+@SpringBootTest(webEnvironment = RANDOM_PORT)
+@Tag("integration")
+@DisplayName("Integration test for Logstash Service")
+@TestPropertySource(properties = { "et.mon.exec=normal" })
+public class LogstashIntegrationTest {
+
+    final Logger log = getLogger(lookup().lookupClass());
+
+    @Autowired
+    LogstashService logstashService;
+
+    WireMockServer wireMockServer;
+
+    @BeforeAll
+    void setup() throws Exception {
+        // Look for free port
+        int port;
+        try (ServerSocket socket = new ServerSocket(0)) {
+            port = socket.getLocalPort();
+        }
+
+        // Mock server for Logstash
+        wireMockServer = new WireMockServer(options().port(port));
+        wireMockServer.start();
+        WireMock.configureFor("localhost", wireMockServer.port());
+
+        // Values injected with Spring properties
+        String mockLogstashUrl = "http://localhost:" + port + "/";
+        FieldSetter.setField(logstashService,
+                LogstashService.class.getDeclaredField("lsHttpApi"),
+                mockLogstashUrl);
+
+        // Stubbing service
+        stubFor(post(urlEqualTo("/")).willReturn(aResponse().withStatus(200)));
+    }
+
+    @Test
+    @DisplayName("Send dummy console logs to mock logstash")
+    void test() {
+        logstashService.sendBrowserConsoleToLogstash("{}", "sessionId");
+        verify(postRequestedFor(urlEqualTo("/")).withHeader("Content-Type",
+                equalTo("application/json; charset=UTF-8")));
+    }
+
+    @AfterAll
+    void teardown() {
+        wireMockServer.stop();
+    }
+
+}
