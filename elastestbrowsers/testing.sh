6ebf66226cb09014dda32a3f38fd3a264df4e593
Fede Diaz <nordri@gmail.com>
2018-03-08 10:34:30 +0100

    removing running container it it exists

--- elastestbrowsers/testing.sh
+++ elastestbrowsers/testing.sh
@@ -6,6 +6,7 @@ DOCKER_OPS="--rm -d --cap-add=SYS_ADMIN -p 4444:4444 -p 5900:5900"
 for BROWSER_VERSION in latest nightly beta 
 do
 	echo "Testing firefox $BROWSER_VERSION"
+	docker rm -f firefox || true
 	docker run --name firefox $DOCKER_OPS elastestbrowsers/firefox:$BROWSER_VERSION
 	sleep 5
 	curl -X POST -d '{"desiredCapabilities":{"browserName":"firefox","version":"","platform":"ANY"}}' http://localhost:4444/wd/hub/session
@@ -15,6 +16,7 @@ done
 for BROWSER_VERSION in latest unstable beta 
 do
 	echo "Testing chrome $BROWSER_VERSION"
+	docker rm -f chrome || true
 	docker run --name chrome $DOCKER_OPS elastestbrowsers/chrome:$BROWSER_VERSION
 	sleep 5
 	curl -X POST -d '{"desiredCapabilities":{"browserName":"chrome","version":"","platform":"ANY"}}' http://localhost:4444/wd/hub/session
