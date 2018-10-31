047a10a8bff032d37adb3bf689d580891d3f4066
nordri <nordri@gmail.com>
2018-10-31 17:46:58 +0100

    New wait method for xvfb using xdpyinfo

--- elastestbrowsers/base/entrypoint.sh
+++ elastestbrowsers/base/entrypoint.sh
@@ -1,24 +1,28 @@
 #!/bin/bash -x
-SCREEN_RESOLUTION=${SCREEN_RESOLUTION:-"1440x1080x24"}
-DISPLAY=99
+export SCREEN_RESOLUTION=${SCREEN_RESOLUTION:-"1440x1080x24"}
+export DISPLAY=99
 
 # Starting selenoid
 /usr/bin/xvfb-run -l -n "$DISPLAY" -s "-ac -screen 0 $SCREEN_RESOLUTION -noreset -listen tcp" /usr/local/bin/selenoid -conf /etc/browsers.json -disable-docker -timeout 1h -enable-file-upload -capture-driver-logs &
 
+export DISPLAY=:99
+
 # Starting VNC
-x11vnc -display ":$DISPLAY" -passwd selenoid -shared -forever -loop500 -rfbport 5900 -rfbportv6 5900 -logfile /home/ubuntu/x11vnc.log &
+x11vnc -display "$DISPLAY" -passwd selenoid -shared -forever -loop500 -rfbport 5900 -rfbportv6 5900 -logfile /home/ubuntu/x11vnc.log &
 
 # Starting novnc
 /usr/local/noVNC/utils/launch.sh --web /usr/local/noVNC --listen 6080 > /home/ubuntu/novnc.log &
 
 # Wait for XVFB
-while [ ! -e /tmp/.X11-unix/X99 ]; do 
-  sleep 1;
-  echo "Waiting xvfb...";
+while ! xdpyinfo >/dev/null 2>&1
+do
+  sleep 0.50s
+  echo "Waiting xvfb..."
 done
 
+
 # Starting fluxbox
-fluxbox -display :$DISPLAY &
+fluxbox -display $DISPLAY >/dev/null 2>&1 &
 
 # Starting pulseaudio 
 pulseaudio -D
