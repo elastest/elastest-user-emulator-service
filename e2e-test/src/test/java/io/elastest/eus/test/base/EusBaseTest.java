bc6a02adeb4d32c76ee07c817eeba69c27416f64
edujgurjc <26252085+EduJGURJC@users.noreply.github.com>
2018-12-20 15:15:02 +0100

    e22 => 1024 window size only in selenoid browsers

--- e2e-test/src/test/java/io/elastest/eus/test/base/EusBaseTest.java
+++ e2e-test/src/test/java/io/elastest/eus/test/base/EusBaseTest.java
@@ -164,19 +164,19 @@ public class EusBaseTest {
 
     protected void navigateTo(WebDriver driver, String url) {
         log.info("Navigating to: {}", url);
-        driver.manage().window().setSize(new Dimension(1024, 1024));
+        if (eusURL == null) {
+            driver.manage().window().setSize(new Dimension(1024, 1024));
+        }
         driver.manage().timeouts().implicitlyWait(5, SECONDS);
         driver.get(url);
     }
 
     protected void navigateToTorm(WebDriver driver) {
         log.info("Navigating to TORM");
-        driver.manage().window().setSize(new Dimension(1024, 1024));
-        driver.manage().timeouts().implicitlyWait(5, SECONDS);
         if (secureElastest) {
-            driver.get(secureTorm);
+            navigateTo(driver, secureTorm);
         } else {
-            driver.get(tormUrl);
+            navigateTo(driver, tormUrl);
         }
     }
 
