b24d0030c0bf65299f2d23ff8f2707efad9db3b8
edujgurjc <26252085+EduJGURJC@users.noreply.github.com>
2019-03-28 14:32:36 +0100

    Try to fix

---
+++ eus/src/test/java/io/elastest/eus/test/config/SchedulingConfiguration.java
@@ -0,0 +1,11 @@
+package io.elastest.eus.test.config;
+
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+@Configuration
+@EnableScheduling
+@ConditionalOnProperty(name = "io.reflectoring.scheduling.enabled", havingValue = "false", matchIfMissing = false)
+public class SchedulingConfiguration {
+}
