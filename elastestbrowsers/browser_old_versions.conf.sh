e2732409c253f3007867e6c158d7c571d773abcc
Juan Navarro <juan.navarro@gmx.es>
2020-03-27 16:12:56 +0100

    elastestbrowsers: fix old versions; rename variables
    * Use same basic name suffix "old_versions" for all things related to
    handling old browser versions, everywhere:
    
    - Rename "browsers_oldreleases" to "browser_old_versions.conf.sh"
    - Rename "Dockerfile.*.older_releases" to "*.old_versions"
    - Rename "browsers.json-oldversions.templ" to
    "browsers.json.templ.old_versions"
    
    * Use clearer variable names:
    
    The existing names were utterly confusing:
    FIREFOX_PKG is not a "package", but a full version number.
    FIREFOX_VER is not a version, but just a major version.
    
    This commit changes all variables to a saner convention:
    <Name>_VERSION for full version numbers (eg. "10.20.30")
    <Name>_VERSION_MAJ for MAJOR version numbers (eg. "10")
    
    * Refactor old version URL downloads outside Dockerfiles, instead pass
    them as a build argument (DOWNLOAD_URL)
    
    * Choose the correct DOWNLOAD_URL for Chrome old versions: the source
    website changed format of the URL starting with Chrome 69.
    
    * Use Bash Arrays for the list of old versions in
    browser_old_versions.conf.sh
    
    * Add safety Bash options for strict error checking in scripts that were
    missing them.

---
+++ elastestbrowsers/browser_old_versions.conf.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+# (shebang not really needed, but required by ShellCheck)
+# shellcheck disable=SC2034
+
+# Version numbers must match those provided here:
+# https://ftp.mozilla.org/pub/firefox/releases/
+FIREFOX_VERSIONS=(
+    57.0.4
+    58.0.2
+    59.0.3
+    60.0.2
+    61.0.2
+    62.0.3
+    63.0.3
+    64.0.2
+    65.0.2
+    66.0.5
+    67.0.4
+    68.0.2
+    69.0.3
+    70.0.1
+    71.0
+    72.0.2
+    73.0.1
+)
+
+# Version numbers must match those provided here:
+# https://www.slimjet.com/chrome/google-chrome-old-version.php
+CHROME_VERSIONS=(
+    60.0.3112.90
+    61.0.3163.79
+    62.0.3202.75
+    63.0.3239.108
+    64.0.3282.140
+    65.0.3325.181
+    66.0.3359.181
+    67.0.3396.79
+    68.0.3440.84
+    69.0.3497.92
+    70.0.3538.77
+    71.0.3578.80
+    75.0.3770.80
+    76.0.3809.100
+    78.0.3904.97
+    79.0.3945.88
+)
--- elastestbrowsers/push.sh
+++ elastestbrowsers/push.sh
@@ -1,45 +1,58 @@
-#!/bin/bash -x
-set -eu -o pipefail
+#!/usr/bin/env bash
+# File checked with ShellCheck (https://www.shellcheck.net/)
 
-WORKDIR=$PWD/workdir
-. $WORKDIR/versions.txt
-. browsers_oldreleases
+# Bash options for strict error checking
+set -o errexit -o errtrace -o pipefail -o nounset
 
-docker push elastestbrowsers/utils-x11-base:${EB_VERSION}
-  
-docker push elastestbrowsers/chrome:latest-${EB_VERSION}
-docker push elastestbrowsers/chrome:latest
-docker push elastestbrowsers/chrome:${CHROME_VER}-${EB_VERSION}
-docker push elastestbrowsers/chrome:${CHROME_VER}
-docker push elastestbrowsers/chrome:beta-${EB_VERSION}
-docker push elastestbrowsers/chrome:beta
-docker push elastestbrowsers/chrome:unstable-${EB_VERSION}
-docker push elastestbrowsers/chrome:unstable
+# Trace all commands
+set -o xtrace
 
-docker push elastestbrowsers/firefox:latest-${EB_VERSION}
-docker push elastestbrowsers/firefox:latest
-docker push elastestbrowsers/firefox:${FIREFOX_VER}-${EB_VERSION}
-docker push elastestbrowsers/firefox:${FIREFOX_VER}
-docker push elastestbrowsers/firefox:beta-${EB_VERSION}
-docker push elastestbrowsers/firefox:beta
-docker push elastestbrowsers/firefox:nightly-${EB_VERSION}
-docker push elastestbrowsers/firefox:nightly
+
+
+# Load old releases versions
+# shellcheck source=browser_old_versions.conf.sh
+source browser_old_versions.conf.sh
+
+# Provide MODE="FULL" to enable generation of all browser versions
+MODE="${MODE:-NIGHTLY}"
+
+# Source "versions.txt" file with browser versions, and download Selenoid
+WORKDIR="$PWD/workdir"
+# shellcheck source=/dev/null
+source "$WORKDIR/versions.txt"
+
+
+
+docker push "elastestbrowsers/utils-x11-base:${EB_VERSION}"
+
+docker push "elastestbrowsers/chrome:${CHROME_VERSION_MAJ}-${EB_VERSION}"
+docker push "elastestbrowsers/chrome:${CHROME_VERSION_MAJ}"
+docker push "elastestbrowsers/chrome:latest-${EB_VERSION}"
+docker push "elastestbrowsers/chrome:latest"
+docker push "elastestbrowsers/chrome:beta-${EB_VERSION}"
+docker push "elastestbrowsers/chrome:beta"
+docker push "elastestbrowsers/chrome:unstable-${EB_VERSION}"
+docker push "elastestbrowsers/chrome:unstable"
+
+docker push "elastestbrowsers/firefox:${FIREFOX_VERSION_MAJ}-${EB_VERSION}"
+docker push "elastestbrowsers/firefox:${FIREFOX_VERSION_MAJ}"
+docker push "elastestbrowsers/firefox:latest-${EB_VERSION}"
+docker push "elastestbrowsers/firefox:latest"
+docker push "elastestbrowsers/firefox:beta-${EB_VERSION}"
+docker push "elastestbrowsers/firefox:beta"
+docker push "elastestbrowsers/firefox:nightly-${EB_VERSION}"
+docker push "elastestbrowsers/firefox:nightly"
 
 if [ "$MODE" == "FULL" ]; then
-	for V in $FIREFOX_OLD_VERSIONS
-	do
-		docker push elastestbrowsers/firefox:$V-${EB_VERSION}
-	done
+  for FIREFOX_VERSION in "${FIREFOX_VERSIONS[@]}"; do
+    FIREFOX_VERSION_MAJ="${FIREFOX_VERSION%%.*}"
+    docker push "elastestbrowsers/firefox:${FIREFOX_VERSION_MAJ}-${EB_VERSION}"
+  done
 
-	OLDVERSIONS=""
-	for VERSION in $CHROME_OLD_VERSIONS
-	do
-		OLDVERSIONS+="$(echo $VERSION | cut -d"." -f1,2) "
-	done
-	for V in $OLDVERSIONS
-	do
-		docker push elastestbrowsers/chrome:$V-${EB_VERSION}
-	done
+  for CHROME_VERSION in "${CHROME_VERSIONS[@]}"; do
+    CHROME_VERSION_MAJ="${CHROME_VERSION%%.*}"
+    docker push "elastestbrowsers/chrome:${CHROME_VERSION_MAJ}-${EB_VERSION}"
+  done
 fi
-  
+
 docker logout
